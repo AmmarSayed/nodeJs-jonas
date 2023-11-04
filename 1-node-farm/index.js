abd855adfa98f5e63660894b5adecdb075af7f4a
Ammar Sayed <ammarsayed1988@gmail.com>
2023-11-04 18:20:28 +0200

    end of lesson 11

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,4 +1,8 @@
 const fs = require("fs");
+const http = require("http");
+
+////////////////////////////////////
+// FILES
 
 // const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 // console.log(textIn);
@@ -8,17 +12,28 @@ const fs = require("fs");
 // console.log("File written");
 
 // non blocking, asynchronous code "Callback hell"
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-    console.log(data2);
-    fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
-      console.log(data3);
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+//       console.log(data3);
 
-      fs.writeFile(`./txt/final.txt`, `${data2}\n\n${data3}`, "utf-8", (err) => {
-        console.log("Your file has been written 😁");
-      });
-    });
-  });
+//       fs.writeFile(`./txt/final.txt`, `${data2}\n\n${data3}`, "utf-8", (err) => {
+//         console.log("Your file has been written 😁");
+//       });
+//     });
+//   });
+// });
+
+// console.log("Last comment");
+
+////////////////////////////////////
+// SERVER
+const server = http.createServer((req, res) => {
+  console.log(req);
+  res.end("Hello from the server");
 });
 
-console.log("Last comment");
+server.listen(8000, "localhost", () => {
+  console.log(`Server is running on port ${8000}!`);
+});

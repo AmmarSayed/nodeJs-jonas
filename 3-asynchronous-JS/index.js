8732d1f4752083e08de24a20e1e91ae077e75fe0
Ammar Sayed <ammarsayed1988@gmail.com>
2023-11-05 23:35:13 +0200

    end of lesson 44

--- 3-asynchronous-JS/index.js
+++ 3-asynchronous-JS/index.js
@@ -39,22 +39,42 @@ const writeFilePro = (file, data) => {
 
 // SOLUTION using promises
 // Will return a promise
-readFilePro(`${__dirname}/dog.txt`)
-  .then((data) => {
-    console.log(`Breed: ${data}`);
-    //superagent will return a promise
-    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
-  })
-  .then((res) => {
-    console.log(res.body.message);
+// readFilePro(`${__dirname}/dog.txt`)
+//   .then((data) => {
+//     console.log(`Breed: ${data}`);
+//     //superagent will return a promise
+//     return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+//   })
+//   .then((res) => {
+//     console.log(res.body.message);
 
-    // will return a promise
-    return writeFilePro('dog-img.txt', res.body.message);
-  })
-  .then((res) => {
-    console.log(res);
+//     // will return a promise
+//     return writeFilePro('dog-img.txt', res.body.message);
+//   })
+//   .then((res) => {
+//     console.log(res);
+//     console.log('Randome dog image saved to file!');
+//   })
+//   .catch((err) => {
+//     return console.log(err.message);
+//   });
+
+// SOLUTION using ASYNC/AWAIT
+const getDogPic = async () => {
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`);
+    console.log(`Breed: ${data}`);
+
+    //superagent will return a promise
+    const dogData = await superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+
+    await writeFilePro('dog-img.txt', dogData.body.message);
     console.log('Randome dog image saved to file!');
-  })
-  .catch((err) => {
-    return console.log(err.message);
-  });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+getDogPic();

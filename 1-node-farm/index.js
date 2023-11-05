c5ac8b1d32a6adb69467ab56c1a3c1fe1d499f87
Ammar Sayed <ammarsayed1988@gmail.com>
2023-11-05 11:58:34 +0200

    end of lesson 17

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const http = require("http");
 const { dirname } = require("path");
 const url = require("url");
+const replaceTemplate = require("./modules/replaceTemplate");
+
 ////////////////////////////////////
 // FILES
 
@@ -30,22 +32,6 @@ const url = require("url");
 
 ////////////////////////////////////
 // SERVER
-
-// Replace template function
-
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-  if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-
-  return output;
-};
 
 // although that readFileSync is a blocking function, it's okay to put it in the global code top level
 // this is because it will be read once in the begninning

b85b1fd068cb86cb170b3faaa9145a0aa88d9bde
Ammar Sayed <ammarsayed1988@gmail.com>
2023-11-05 11:37:18 +0200

    end of lesson 15

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -30,23 +30,57 @@ const url = require("url");
 
 ////////////////////////////////////
 // SERVER
+
+// Replace template function
+
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+  if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+
+  return output;
+};
+
 // although that readFileSync is a blocking function, it's okay to put it in the global code top level
 // this is because it will be read once in the begninning
 // also to avoid reading the entire file each time somebody visits the API route
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObject = JSON.parse(data);
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, "utf-8");
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, "utf-8");
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, "utf-8");
 
 const server = http.createServer((req, res) => {
   const pathname = req.url;
+
+  // overview page
   if (pathname === "/" || pathname === "/overview") {
-    res.end("This is the OVERVIEW");
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+
+    const cardsHTML = dataObject.map((el) => replaceTemplate(tempCard, el)).join("");
+
+    res.end(tempOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHTML));
+
+    // product page
   } else if (pathname === "/product") {
     res.end("this is the PRODUCT");
+
+    // API
   } else if (pathname === "/api") {
     res.writeHead(200, {
       "Content-type": "application/json",
     });
     res.end(data);
+
+    // not found
   } else {
     res.writeHead(404, {
       "Content-Type": "text/html",

dab396054a1a596ff483ac18080f9b4bd928d456
Ammar Sayed <ammarsayed1988@gmail.com>
2023-11-16 07:12:16 +0200

    Stopped tracking .env File

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,7 +1,20 @@
 // order matters
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log(`DB connection successfull`);
+  })
+  .catch((err) => console.log(err));
 
 const PORT = process.env.PORT || 3000;
 
